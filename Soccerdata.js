ace3a9affabcd44c2143e790b744d855e11dd064
eirik <fosnaeseirik@seattleu.edu>
2019-04-18 12:22:01 -0700

    hw files

---
+++ Soccerdata.js
@@ -0,0 +1,139 @@
+asd{
+  "data": [
+    {
+      "name": "Harry Kane",
+      "Team": "Tottenham",
+      "week_salary": "$200,000",
+      "expiration_date": "2024/06/30",
+      "Goals_season": "17",
+      "assists_season": "4",
+      "games_played": "29"
+    },
+    {
+      "name": "Sergio Aguero",
+      "Team": "Man City",
+      "week_salary": "$220,000",
+      "expiration_date": "2021/06/30",
+      "Goals_season": "19",
+      "assists_season": "7",
+      "games_played": "30"
+    },
+    {
+      "name": "Ashley Barnes",
+      "Team": "Burnley",
+      "week_salary": "$20,000",
+      "expiration_date": "2019/30/06",
+      "Goals_season": "10",
+      "assists_season": "2",
+      "games_played": "33"
+    },
+    {
+      "name": "Mo Salah",
+      "Team": "Liverpool",
+      "week_salary": "$200,000",
+      "expiration_date": "2023/06/30",
+      "Goals_season": "19",
+      "assists_season": "7",
+      "games_played": "34"
+    },
+    {
+      "name": "Pierre-Emerick Aubameyang",
+      "Team": "Arsenal",
+      "week_salary": "$180,000",
+      "expiration_date": "2021/06/30",
+      "Goals_season": "18",
+      "assists_season": "5",
+      "games_played": "32"
+    },
+    {
+      "name": "Paul Pogba",
+      "Team": "Manchester United",
+      "week_salary": "$290,000",
+      "expiration_date": "2021/06/30",
+      "Goals_season": "13",
+      "assists_season": "9",
+      "games_played": "30"
+    },
+    {
+      "name": "Mesut Ozil",
+      "Team": "Arsenal",
+      "week_salary": "$350,000",
+      "expiration_date": "2021/06/30",
+      "Goals_season": "4",
+      "assists_season": "2",
+      "games_played": "21"
+    },
+    {
+      "name": "Alexis Sanchez",
+      "Team": "Manchester United",
+      "week_salary": "$350,000",
+      "expiration_date": "2022/06/30",
+      "Goals_season": "1",
+      "assists_season": "3",
+      "games_played": "17"
+    },
+    {
+      "name": "N'Golo Kanté",
+      "Team": "Chelsea",
+      "week_salary": "$290,000",
+      "expiration_date": "2023/06/30",
+      "Goals_season": "3",
+      "assists_season": "4",
+      "games_played": "33"
+    },
+    {
+      "name": "Virgil Van Dijk",
+      "Team": "Liverpool",
+      "week_salary": "$180,000",
+      "expiration_date": "2023/06/30",
+      "Goals_season": "3",
+      "assists_season": "2",
+      "games_played": "34"
+    },
+    {
+      "name": "Heung-Min Son",
+      "Team": "Tottenham",
+      "week_salary": "$140,000",
+      "expiration_date": "2023/06/30",
+      "Goals_season": "12",
+      "assists_season": "7",
+      "games_played": "27"
+    },
+    {
+      "name": "Jamie Vardy",
+      "Team": "Leicester City",
+      "week_salary": "$120,000",
+      "expiration_date": "2022/06/30",
+      "Goals_season": "15",
+      "assists_season": "4",
+      "games_played": "30"
+    },
+    {
+      "name": "Ryan Fraser",
+      "Team": "Bournemouth",
+      "week_salary": "$5,000",
+      "expiration_date": "2020/06/30",
+      "Goals_season": "7",
+      "assists_season": "12",
+      "games_played": "34"
+    },
+    {
+      "name": "Christian Eriksen",
+      "Team": "Tottenham",
+      "week_salary": "$75,000",
+      "expiration_date": "2020/06/30",
+      "Goals_season": "6",
+      "assists_season": "12",
+      "games_played": "30"
+    },
+    {
+      "name": "Leroy Sane",
+      "Team": "Man City",
+      "week_salary": "$90,000",
+      "expiration_date": "2021/06/30",
+      "Goals_season": "9",
+      "assists_season": "11",
+      "games_played": "27"
+    }
+  ]
+}
